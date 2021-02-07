a073f3d35df50a10d1ddae415bd613c2a258b85a
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-07 19:55:18 +0000

    Add material-ui and implement some initial styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import Header from './components/Header';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+
+    backgroundColor: '#202430',
+    color: 'whitesmoke',
+
+    minHeight: '100vh',
+    width: '100%',
+  },
+});
 
 function App() {
-  return <h1>hello world</h1>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <CssBaseline />
+
+      <div className={classes.root}>
+        <Header />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { AppBar, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appbar: {
+    background: 'none',
+    alignItems: 'center',
+  },
+
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+    fontWeight: 400,
+    fontSize: '24px',
+    fontFamily: "'Josefin Sans', sans-serif",
+    letterSpacing: '1px',
+  },
+
+  logo: {
+    width: '150px',
+    height: '50px',
+    margin: '0 15px',
+  },
+});
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.appbar}>
+      <h1 className={classes.title}>
+        My
+        <img
+          className={classes.logo}
+          src={process.env.PUBLIC_URL + '/images/adeptly.png'}
+          alt="Adeptly"
+        />
+        Application
+      </h1>
+    </AppBar>
+  );
+}
+
+export default Header;
